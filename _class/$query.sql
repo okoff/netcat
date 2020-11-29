10ceecc45c890c4331bf81ac674e59f13103e297
okoff <okoff@yandex.ru>
2020-11-30 02:59:31 +0300

    57 updated

--- _class/$query.sql
+++ _class/$query.sql
@@ -477,15 +477,17 @@ if (!$admin_mode) {
 echo \"
 <script src=\'https://code.jquery.com/jquery-3.3.1.min.js\'></script>
 <script src=\'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js\' integrity=\'sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ\' crossorigin=\'anonymous\'></script>
-<!-- 
 <script src=\'https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js\' integrity=\'sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm\' crossorigin=\'anonymous\'></script>
+<!-- 
 <script src=\'/popup-win/js/jquery.maskedinput.min.js\'></script>
 -->
 \";
 
-$quickorder_name = $NETCAT_FOLDER.\"popup-win\\quickorder_popup.inc.html\";
+$quickorder_name = $NETCAT_FOLDER.\"popup-win/quickorder_popup.inc.html\";
 $quickorder_form = file_get_contents($quickorder_name);
-//error_log(\"templatefull name:\".$quickorder_name.\" \".$quickorder_form);
+
+echo \"<!-- «агрузка формы быстрого заказа (\".$quickorder_name.\") размер:\".strlen($quickorder_form).\" -->
+\"; 
 if ($quickorder_form) {
 	echo $quickorder_form;
 }
